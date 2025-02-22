17761e5e07aa7cf25e376bba976701b70f1e8d5d
ajjget <ajjget@gmail.com>
2025-02-21 22:51:16 -0700

    calculator interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,49 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Testing dockerfile");
+    Scanner scanner = new Scanner(System.in);
+    Calcultor calculator = new Calculator();
+
+    while (scanner.hasNext()) {
+      String command = scanner.next();
+
+      switch (command) {
+        case "add":
+          int a1 = scanner.nextInt();
+          int b1 = scanner.nextInt();
+          System.out.println(calculator.add(a1, b1));
+          break;
+        case "subtract":
+          int a2 = scanner.nextInt();
+          int b2 = scanner.nextInt();
+          System.out.println(calculator.subtract(a2, b2));
+          break;
+        case "multiply":
+          int a3 = scanner.nextInt();
+          int b3 = scanner.nextInt();
+          System.out.println(calculator.multiply(a3, b3));
+          break;
+        case "divide":
+          int a4 = scanner.nextInt();
+          int b4 = scanner.nextInt();
+          if (b4 == 0) {
+            System.out.println("Error: Division by zero");
+          } else {
+            System.out.println(calculator.divide(a4, b4));
+          }
+          break;
+        case "fibonacci":
+          int n = scanner.nextInt();
+          System.out.println(calculator.fibonacciNumberFinder(n));
+          break;
+        case "binary":
+          int number = scanner.nextInt();
+          System.out.println(calculator.intToBinaryNumber(number));
+          break;
+        default:
+          System.out.println("Invalid command");
+          break;
+      }
+    }
+    scanner.close();
   }
 }
