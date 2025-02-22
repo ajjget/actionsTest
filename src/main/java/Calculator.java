a38f3bb1ab16d8c5d47d2fe9f8e595a3c82adcf5
ajjget <ajjget@gmail.com>
2025-02-21 22:47:53 -0700

    unfail

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -38,17 +38,16 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-//        if (n <= 0) return 0;
-//        if (n == 1 || n == 2) return 1;
-//
-//        int a = 1, b = 1;
-//        for (int i = 3; i <= n; i++) {
-//            int temp = a + b;
-//            a = b;
-//            b = temp;
-//        }
-//        return b;
-        return n;
+        if (n <= 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int a = 1, b = 1;
+        for (int i = 3; i <= n; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
     }
 
 
